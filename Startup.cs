d025c74554b3da041a9184ff4f0e510a6eeab3c1
David <rahrahfez@hotmail.com>
2019-10-29 00:36:16 -0400

    added favicon

--- Startup.cs
+++ Startup.cs
@@ -48,6 +48,7 @@ namespace ListChallengeApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStaticFiles();
             app.UseCors(MyAllowSpecificOrigins);
             // app.UseHttpsRedirection();
             app.UseMvc();
